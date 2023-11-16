0e64b4b3fe086e686148730f786d99cc12f7528f
Linincun <103359058+Linincun@users.noreply.github.com>
2023-11-16 22:02:38 +0800

    Update my_train.py

--- my_train.py
+++ my_train.py
@@ -1,12 +1,12 @@
 from ultralytics import YOLO
 
 # Create a new YOLO model from scratch
-model = YOLO('/root/code/ultralytics/cfg/models/v8/myyolov8.yaml')
+model = YOLO('/root/code/myyolo/ultralytics/cfg/models/v8/myyolov8.yaml')
 
 # Load a pretrained YOLO model (recommended for training)
-model = YOLO('/root/code/yolov8n.pt')
+model = YOLO('/root/code/myyolo/yolov8n.pt')
 
 if __name__ == '__main__':
     # Use the model
-    results = model.train(data='/root/code/ultralytics/cfg/datasets/mycoco128.yaml',workers=1, epochs=300,batch=32)  # train the model
-    success = YOLO('/root/code/yolov8n.pt').export(format="onnx")  # export a model to ONNX format
+    results = model.train(data='/root/code/myyolo/ultralytics/cfg/datasets/mycoco128.yaml',workers=1, epochs=300,batch=32)  # train the model
+    success = YOLO('/root/code/myyolo/yolov8n.pt').export(format="onnx")  # export a model to ONNX format
